990d516e27ad6fa33691ab00a049d240d02e17a6
Vladimir Blazek <blazevl1@fit.cvut.cz>
2012-02-01 14:51:28 +0100

    HW4 - Decipher

--- Decipher/lib/main.rb
+++ Decipher/lib/main.rb
@@ -4,11 +4,13 @@ require 'decipher.rb'
 if (ARGV.length != 1)
   puts "Musite zadat soubor pro desifrovani jako parametr"
   puts "Napriklad: main.rb input.txt"
+else
+  decipher = Decipher.new
+  begin
+    puts decipher.decipher_file(ARGV[0])
+  rescue
+    puts "Zadany soubor #{ARGV[0]} neexistuje"
+  end
 end
 
-decipher = Decipher.new
-begin
-  puts decipher.decipher_file(ARGV[0])
-rescue
-  puts "Zadany soubor #{ARGV[0]} neexistuje"
-end
+
