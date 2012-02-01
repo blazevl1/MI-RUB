9ce23a03f8f856a07f1d8cf1bfbca7e65cf0538d
Vladimir Blazek <blazevl1@fit.cvut.cz>
2012-02-01 14:51:57 +0100

    HW5 - The Minimal Coverage

--- hw5/lib/main.rb
+++ hw5/lib/main.rb
@@ -3,6 +3,16 @@ require 'solver'
 
 include MinimalCoverage
 
+if (ARGV.length != 1)
+  puts "Musite zadat soubor jako parametr"
+  puts "Napriklad: main.rb input.txt"
+else
+  if (File.exist?(ARGV[0]))
+    solver = Solver.new
+    puts solver.solve(ARGV[0])
+  else
+    puts "Zadany soubor #{ARGV[0]} neexistuje!"
+  end
+end
 
-solver = Solver.new
-puts solver.solve("input_2.txt")
+
