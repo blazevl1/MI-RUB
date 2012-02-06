20a7b7c9ee92049cc2daf01ec0180b980bdb8545
Vladimir Blazek <blazevl1@fit.cvut.cz>
2012-02-06 13:03:13 +0100

    Deleting invalid directories

--- hw1/spec/spec_helper.rb
+++
@@ -1,2 +0,0 @@
-require_relative '../rectangle.rb'
-require_relative '../union_calculator.rb'
--- hw5/spec/spec_helper.rb
+++
@@ -1,4 +0,0 @@
-require_relative '../lib/segment.rb'
-require_relative '../lib/file_parser.rb'
-require_relative '../lib/test_case.rb'
-require_relative '../lib/solver.rb'
